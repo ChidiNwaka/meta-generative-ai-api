3e22a4f920344e4b414994d9e79cc19d1b3498f1
Chidi Nwaka <chidinwaka@Chidis-MacBook-Pro.local>
2024-02-25 06:59:54 -0800

    initial commiti

---
+++ index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+
+import express  from 'express';
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port http://localhost:${port}`)
+})
+
+
