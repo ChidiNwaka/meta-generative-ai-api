65090220fe2b306b0d902fc6cffef74062cb58af
Chidi Nwaka <chidi.nwaka@gmail.com>
2024-03-09 08:30:31 -0800

    Installed express using the generator

---
+++ app.js
@@ -0,0 +1,50 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+// TODO: Move port value to .env
+const port = 3000
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// TODO: Update the view engine below to pug (the package already installed)
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);    
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`listening on port http://localhost:${port}`);
+});
+
+module.exports = app;
